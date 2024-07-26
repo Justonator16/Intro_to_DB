6362d5b58700ef29a3049760194665cd0ed492cd
Junior <juniorjusto16@gmail.com>
2024-07-26 16:46:50 +0200

    Create database using python and mysql connector

---
+++ MySQLServer.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="QaZWSXEDCRFVTGB"
+    )
+
+    mycursor = mydb.cursor()
+
+    result = mycursor.execute("CREATE DATABASE IF NOT EXIST alx_book_store")
+
+    print("Database 'alx_book_store' created successfully!")
+
+    
+except:
+    print("An error occured")
