8222dd155a207fbf7cd7c7890bb80b8c8efbaf05
Junior <juniorjusto16@gmail.com>
2024-07-26 16:12:27 +0200

    Book fixed

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -12,7 +12,7 @@ CREATE TABLE Books(
   	author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-  	FOREIGN KEY (author_id) REFERENCING Authors(author_id)
+  	FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Customers(
@@ -27,7 +27,7 @@ CREATE TABLE Orders(
     order_id AUTO_INCREMENT PRIMARY KEY,
   	order_date DATE NOT NULL,
     customer_id INT,
-    FOREIGN KEY (customer_id) REFERENCING Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
     
 );
 
@@ -35,6 +35,6 @@ CREATE TABLE Order_details(
     order_id INT,
   	book_id INT,
   	quantity DOUBLE NOT NULL,
-    FOREIGN KEY order_id REFERENCING Orders(order_id),
-    FOREIGN KEY book_id REFERENCING Books(book_id)
+    FOREIGN KEY order_id REFERENCES Orders(order_id),
+    FOREIGN KEY book_id REFERENCES Books(book_id)
 );
